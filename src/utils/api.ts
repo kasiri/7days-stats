6d45fa75fbe7066935de7af944c6fbca25251ca5
Kasiri <kasiri2893@gmail.com>
2026-03-12 13:52:07 +0100

    ApiCorrecta?

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,13 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function fetchStats(): Promise<any> {
-  const res = await fetch("/api/stats.json");
+  const res = await fetch("https://api.kasiriserver.space/stats");
   if (!res.ok) throw new Error(res.statusText);
   return res.json();
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function fetchRanking(): Promise<any> {
-  const res = await fetch("/api/ranking.json");
+  const res = await fetch("https://api.kasiriserver.space/ranking");
   if (!res.ok) throw new Error(res.statusText);
   return res.json();
 }
